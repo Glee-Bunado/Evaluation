3836f9183db15ef882b615a95ebdf9201e44c123
Glee-Bunado <gjbunado@agiletech.ph>
2022-12-14 16:51:34 +0800

    updated evaluation codes

--- investment/models/investment_benefit.py
+++ investment/models/investment_benefit.py
@@ -1,4 +1,7 @@
 from odoo import fields, models, api
+from datetime import date
+import datetime
+
 
 class InvestmentBenefit(models.Model):
     _name = "investment.benefit"
@@ -9,13 +12,30 @@ class InvestmentBenefit(models.Model):
     date_start = fields.Date(string="Start Date")
     date_end = fields.Date(string="End Date")
     benefit_value = fields.Float(compute="_benefit_amt")
-    investment_ids = fields.One2many("investment.module", "benefit_id")
+    investment_id = fields.Many2one("investment.module")
 
-
-    @api.depends("investment_ids")
+    @api.depends("investment_id")
     def _benefit_amt(self):
         for rec in self:
-            rec.benefit_value = 0.50 * rec.investment_ids.price
+            # benefit rate for the following investments
+            invest_search = self.env["investment.benefit"].search([('partner_id', '=', rec.partner_id.id)])
+            x = []
+            for inv in invest_search:
+                x.append(inv.investment_id.investment_amount)
+
+            first_benefit_value = 0.50 * x[0]
+            following_benefit_rate = sum(x)/rec.investment_id.total_investments
+
+            d_start = date(rec.date_start.year, rec.date_start.month, rec.date_start.day)
+            d_end = date(rec.date_end.year, rec.date_end.month, rec.date_end.day)
+            d_deposit = date(rec.investment_id.date.year, rec.investment_id.date.month, rec.investment_id.date.day)
+
+            if 0 < (d_end - d_start).days <= 15:
+                rec.benefit_value = first_benefit_value
+
+            if (d_end - d_start).days > 15:
+                rec.benefit_value = rec.investment_id.next_investments * following_benefit_rate
+
 
 
 
--- investment/models/investment_module.py
+++ investment/models/investment_module.py
@@ -11,4 +11,36 @@ class InvestmentModule(models.Model):
     investment_amount = fields.Float(string="Investment Amount")
     currency_id = fields.Many2one("res.currency", string="Currency")
     partner_id = fields.Many2one("res.partner")
-    benefit_id = fields.Many2one("investment.benefit")
+    total_invested = fields.Float(string="Total Invested Amount", compute="_total_invested")
+    total_investments = fields.Float(string="Total Investments", compute="_total_investments")
+    first_investment = fields.Float(compute="_first_investment")
+    next_investments = fields.Float(compute="_next_investments")
+
+    @api.depends("investment_amount")
+    def _total_invested(self):
+        for rec in self:
+            invest_search = self.env["investment.module"].search([('partner_id', '=', rec.partner_id.id)])
+            rec.total_invested = sum([inv.investment_amount for inv in invest_search])
+
+    @api.depends("investment_amount")
+    def _total_investments(self):
+        total = sum(self.env["investment.module"].search([]).mapped('investment_amount'))
+        for rec in self:
+            rec.total_investments = total
+
+    @api.depends("investment_amount")
+    def _first_investment(self):
+        for rec in self:
+            invest_search = self.env["investment.module"].search([('partner_id', '=', rec.partner_id.id)])
+            x = []
+            for inv in invest_search:
+                x.append(inv.investment_amount)
+            rec.first_investment = x[0]
+    @api.depends("investment_amount")
+    def _next_investments(self):
+        for rec in self:
+            invest_search = self.env["investment.module"].search([('partner_id', '=', rec.partner_id.id)])
+            x = []
+            for inv in invest_search:
+                x.append(inv.investment_amount)
+            rec.next_investments = sum(x[1:])
